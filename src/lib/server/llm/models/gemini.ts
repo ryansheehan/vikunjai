51044f9f051116a4fedc263287fcd42161e52b61
plap <paul.lapczynski@gmail.com>
2025-06-13 15:45:43 -0400

    chore: remove bloat

--- src/lib/server/llm/models/gemini.ts
+++ src/lib/server/llm/models/gemini.ts
@@ -1,28 +1,11 @@
-import { ChatGoogleGenerativeAI } from '@langchain/google-genai';
-import { ChatVertexAI} from "@langchain/google-vertexai";
-import { ChatOpenAI } from "@langchain/openai";
-import { env } from "$env/dynamic/private";
-import * as process from "process"
+import {ChatGoogleGenerativeAI} from '@langchain/google-genai';
+import {env} from "$env/dynamic/private";
 
 export function createGeminiModel() {
-    console.log(process.env.GOOGLE_API_KEY);
     const model = new ChatGoogleGenerativeAI({
         model: "gemini-2.0-flash-lite",
         temperature: 0,
-        apiKey: process.env.GOOGLE_API_KEY,
-    });
-    return model;
-}
-
-
-export function createGemmaModel() {
-    const model = new ChatOpenAI({
-        model: "google/gemma-3-12b",
-        temperature: 0,
-        configuration: {
-            baseURL: "http://localhost:1234/v1",
-            apiKey: 'not needed'
-        }
+        apiKey: env.GOOGLE_API_KEY,
     });
     return model;
 }
